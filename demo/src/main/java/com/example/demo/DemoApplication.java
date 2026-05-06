d96e82df93dd0ee6889b8a0a3b8ccf3a3815db45
alejandroblazquez5 <alejandro.blazquez@des.kyocera.com>
2026-05-06 12:03:28 +0200

    Version comentada

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,4 +10,18 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	/*
+	Para iniciar la pagina web:
+		1. Encender MYSQL desde docker, en el puerto 3306
+		2. Abrir dberaver para comprobar que se conecta 
+		  (en caso de no tener ninguna base de datos creada, dentro de la carpeta del proyecto hay un archivo sql para hacer la base de datos,
+		   del archivo sql solo se ejecuta hasta la linea 206, el resto son solo para arreglar fallos si es necesario)
+		3. Correr este programa, cuando el terminal diga: "Started DemoApplication" estara preparada 
+		4. Hostear la pagina administracion.html desde visual studio code con la extension: "Live Server"
+	Con esto la pagina web ya funcionaria completamente.
+	
+	Usuario y contraseña de la base de datos:
+	 - Usuario: "root"
+	 - Contraseña: "root"
+	*/
 }
